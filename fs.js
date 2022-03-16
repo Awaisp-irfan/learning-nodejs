f589f5e611c091fa50a127ef3bcdf2ec7cb4d47b
Awaisp-irfan <irfanmaqsood.im123@gmail.com>
2022-03-16 23:18:45 +0500

    write file read file

--- fs.js
+++ fs.js
@@ -1,13 +1,13 @@
 const fs= require('fs')
-// console.log(fs);
+console.log(fs);
 
 // read file
-// fs.readFile('./docs/txt.txt' , (err , data) =>{   // it's a async function
-//     if (err){
-//         console.log(err);
-//     }
-//     console.log(data);
-// })
+fs.readFile('./docs/txt.txt' , (err , data) =>{   // it's a async function
+    if (err){
+        console.log(err);
+    }
+    console.log(data);
+})
 // write file 
 fs.writeFile('./docs/txt2.txt' , 'Lorem dolor.' , ()=>{
     console.log('file was written')
